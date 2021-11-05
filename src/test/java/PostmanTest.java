f31a6af57f64400d44e4ad8bd3cb7d973a520128
Vera Preymachuk <veragen@mail.ru>
2021-11-05 13:51:17 +0700

    Initial commit

---
+++ src/test/java/PostmanTest.java
@@ -0,0 +1,20 @@
+import static io.restassured.RestAssured.given;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanTest {
+    @Test
+    void shouldTestPost() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("user id")
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("user id"));
+    }
+}
